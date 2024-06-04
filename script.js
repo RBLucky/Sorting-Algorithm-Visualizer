0c9edd9b542d1ef4b28da5b3d4a0ba143869edfa
RBLucky <nkosi.s.lucky@gmail.com>
2024-06-04 20:30:39 +0200

    animate bubble sort

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
-const n = 10;
+const n = 30;
 const array = [];
 
-for (let i = 0; i < n; i++) {
-    array[i]=Math.random();
-}
+init();
 
-do {
-    var swapped = false;
-    for (let i = 1; i < array.length; i++) {
-        if (array[i-1] > array[i]) {
-            swapped = true;
-            [array[i-1], array[i]] = [array[i], array[i-1]];
-        }
+function init() {
+    for (let i = 0; i < n; i++) {
+        array[i] = Math.random();
     }
-} while (swapped);
-
-for (let i=0; i < array.length; i++) {
-    const bar = document.createElement("div");
-    bar.style.height = array[i]*100 + "%";
-    bar.classList.add("bar");
-    container.appendChild(bar);
+    showBars();
 }
 
-//createElement()
-//appendChild()
-//classList.add()
+function play() {
+    const copy = [...array];
+    const swaps = bubbleSort(copy);
+    animate(swaps);
+}
+
+function animate(swaps) {
+    if (swaps.length == 0) {
+        showBars();
+        return;
+    }
+    const [i,j] = swaps.shift();
+    [array[i], array[j]] = [array[j], array[i]];
+    showBars([i,j]);
+    setTimeout(function(){
+        animate(swaps);
+    }, 200);
+}
+
+function bubbleSort(array) {
+    const swaps = [];
+    do {
+        var swapped = false;
+        for (let i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                swapped = true;
+                swaps.push([i-1,i]);
+                [array[i - 1], array[i]] = [array[i], array[i - 1]];
+            }
+        }
+    } while (swapped);
+    return swaps;
+}
+
+function showBars(indices) {
+    container.innerHTML="";
+    for (let i = 0; i < array.length; i++) {
+        const bar = document.createElement("div");
+        bar.style.height = array[i] * 100 + "%";
+        bar.classList.add("bar");
+
+        if (indices && indices.includes(i)) {
+            bar.style.backgroundColor = "red";
+        }
+        container.appendChild(bar);
+    }
+}
+
+//(21:23)
+//.createElement()
+//.appendChild()
+//.classList.add()
+//destructuring assignment
+//.shift()
+//setTimeout()
+//[...array]
+//.includes()
